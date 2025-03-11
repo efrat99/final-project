b89c972fa1620246de3cf450979e259c2e1272d9
shanirap <s0548454092@gmail.com>
2025-03-12 01:28:12 +0200

    UpdateCLIENT

---
+++ Client/src/Components/Student.jsx
@@ -0,0 +1,21 @@
+import { Card } from 'primereact/card';
+import axios from 'axios'
+
+const teacher=(prop)=>{ 
+    
+    
+    return(
+        <div className="card flex justify-content-center" >
+        <Card footer={footer} header={header} style={{borderColor: 'rgb(120, 180, 180)', borderStyle: 'double', borderWidth:'2px'}}>
+            {props.teacher.firstName}
+            <br></br>
+            {props.user.lastName}
+            <br></br>
+            {props.user.email}
+            <br></br>
+            {props.user.phone}
+
+        </Card>
+    </div>
+    )
+}
--- Client/src/index.js
+++ Client/src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'primereact/resources/themes/lara-light-blue/theme.css';  // ערכת נושא
+import 'primereact/resources/primereact.min.css';               // סגנונות בסיסיים
+import 'primeicons/primeicons.css';                             // אייקונים
+//import 'primeflex/primeflex.css';                               // מערכת גריד ועיצוב
+
+
 // import reportWebVitals from './reportWebVitals';
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <PrimeReactProvider>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </PrimeReactProvider>
 );
 // reportWebVitals();
